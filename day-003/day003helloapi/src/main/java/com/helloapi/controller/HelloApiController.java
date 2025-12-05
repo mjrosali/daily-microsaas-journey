32abc471c52dec77b345777807d53c9495a26853
Maria Jenny Rosali <mjrosali@Marias-MacBook-Air.local>
2025-12-05 17:36:42 +0800

    day 3: feat: added hello endpoint with path variable

--- day-003/day003helloapi/src/main/java/com/helloapi/controller/HelloApiController.java
+++ day-003/day003helloapi/src/main/java/com/helloapi/controller/HelloApiController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api2")
 public class HelloApiController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public Map<String, String> hello() {
         Map<String, String> helloapi = new HashMap<>();
         helloapi.put("message", "hello from api!");
+        return helloapi;
+    }
+
+    @GetMapping("/hello/{name}")
+    public Map<String, String> hello(@PathVariable String name) {
+        Map<String, String> helloapi = new HashMap<>();
+        helloapi.put("message", "hello " + name + " from api!");
         return helloapi;
     }
 }
